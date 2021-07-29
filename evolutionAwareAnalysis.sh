f88fd349edbd2f390a42c0fa8d9b9d7204a99abf
brenoafb <breno.afb@gmail.com>
2021-07-29 01:07:36 -0300

    Apply shellcheck suggestions

--- evolutionAwareAnalysis.sh
+++ evolutionAwareAnalysis.sh
@@ -30,7 +30,7 @@ COMMAND="java -Xms$xms -Xmx$xmx -jar $JAR"
 # Path were we dump and read ADDs for previous analyses
 ANALYSES_PATH=Analyses/$spl
 PERSISTED_ANALYSES_PATH=PersistedAnalyses/$spl
-rm -rf $PERSISTED_ANALYSES_PATH
+rm -rf "$PERSISTED_ANALYSES_PATH"
 # Path were we save analysis stats
 LOGS_DIR=$ANALYSES_PATH/logs
 MEMORY_DIR=$ANALYSES_PATH/logs/data/memory_usage
@@ -44,11 +44,11 @@ echo "1st step - analysis of the original spl"
 # TODO: we have to parameterize the variableStore's location
 rm variableStore.add
 
-mkdir -p $PERSISETD_ANALYSES_PATH
-mkdir -p $ANALYSES_PATH
-mkdir -p $LOGS_DIR
-mkdir -p $MEMORY_DIR
-mkdir -p $TIME_DIR
+mkdir -p "$PERSISTED_ANALYSES_PATH"
+mkdir -p "$ANALYSES_PATH"
+mkdir -p "$LOGS_DIR"
+mkdir -p "$MEMORY_DIR"
+mkdir -p "$TIME_DIR"
 
 eval "$COMMAND --all-configurations --uml-model=$spl/bm$spl$initial_evolution.xml --feature-model=$spl/fm$spl$initial_evolution.txt --persisted-analyses=$PERSISTED_ANALYSES_PATH"
 
@@ -57,20 +57,20 @@ eval "$COMMAND --all-configurations --uml-model=$spl/bm$spl$initial_evolution.xm
 
 for i in $(seq 1 $iterations ); do
 	for e in $(seq $(expr $initial_evolution + 1) $final_evolution); do
-		mkdir -p $LOGS_DIR/$i
-		echo ----------   Iteration $i     Evolution $e   ----------
+		mkdir -p "$LOGS_DIR/$i"
+		echo "----------   Iteration $i     Evolution $e   ----------"
 		eval "$COMMAND --all-configurations --uml-model=$spl/bm$spl$e.xml --feature-model=$spl/fm$spl$e.txt --persisted-analyses=$PERSISTED_ANALYSES_PATH >> $LOGS_DIR/$i/evolution$e.out"
 	done
 done
 
 # 3rd step: recover all analyses and total times of all evolutions
 for e in $(seq $(expr $initial_evolution + 1) $final_evolution); do
-	echo ---------- Evolution $e ---------- >> $LOGS_DIR/analysisTime.out
-	echo ---------- Evolution $e ---------- >> $MEMORY_LOG_FILE
-	echo ---------- Evolution $e ---------- >> $TIME_LOG_FILE
+	echo "---------- Evolution $e ----------" >> "$LOGS_DIR/analysisTime.out"
+	echo "---------- Evolution $e ----------" >> "$MEMORY_LOG_FILE"
+	echo "---------- Evolution $e ----------" >> "$TIME_LOG_FILE"
 	for i in $(seq 1 $iterations ); do
-		cat $LOGS_DIR/$i/evolution$e.out | grep "Total analysis" | awk '{print $4}' >> $LOGS_DIR/analysisTime.out
-		cat $LOGS_DIR/$i/evolution$e.out | grep "Total running" | awk '{print $4}' >> $TIME_LOG_FILE
-		cat $LOGS_DIR/$i/evolution$e.out | grep "Total memory" | awk '{print $4}' >> $MEMORY_LOG_FILE
+		cat "$LOGS_DIR/$i/evolution$e.out" | grep "Total analysis" | awk '{print $4}' >> "$LOGS_DIR/analysisTime.out"
+		cat "$LOGS_DIR/$i/evolution$e.out" | grep "Total running" | awk '{print $4}' >> "$TIME_LOG_FILE"
+		cat "$LOGS_DIR/$i/evolution$e.out" | grep "Total memory" | awk '{print $4}' >> "$MEMORY_LOG_FILE"
 	done
 done
