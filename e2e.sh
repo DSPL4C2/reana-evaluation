c40c69255d6966e1286401689766be35f026af44
brenoafb <breno.afb@gmail.com>
2021-08-03 21:52:10 -0300

    copy results to directory after executing

--- e2e.sh
+++ e2e.sh
@@ -6,6 +6,7 @@ n_iterations=$3
 
 EVAL_DIR=/reana-evaluation
 ANALYSIS_DIR=/reana-data
+OUTPUT_DIR=/root/results
 
 bash "$EVAL_DIR/evolutionObliviousAnalysis.sh" "$spl" "$n_evolutions" "$n_iterations"
 bash "$EVAL_DIR/evolutionAwareAnalysis.sh" "$spl" "$n_evolutions" "$n_iterations"
@@ -13,3 +14,5 @@ bash "$EVAL_DIR/evolutionAwareAnalysis.sh" "$spl" "$n_evolutions" "$n_iterations
 cp -r $EVAL_DIR/Analysis/logs/data $ANALYSIS_DIR/datasets/data
 
 python3 $ANALYSIS_DIR/analyse-cli.py
+
+cp -r $ANALYSIS_DIR/results $OUTPUT_DIR
